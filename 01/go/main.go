7ea782dc795bdf9a331727e8ef6f9ccdd73394ec
Pantonshire <tom@tomandtally.co.uk>
2020-12-01 11:35:17 +0000

    Day 1 optimisations

--- 01/go/main.go
+++ 01/go/main.go
@@ -30,14 +30,14 @@ func main() {
     panic(err)
   }
 
-  for _, r := range findTwo(10, ns) {
+  for _, r := range findTwo(ns) {
     n0, n1 := ns[r.i0], ns[r.i1]
     sum, prod := n0+n1, n0*n1
     fmt.Println(fmt.Sprintf("indices (%d,%d): %d + %d = %d, %d * %d = %d",
       r.i0, r.i1, n0, n1, sum, n0, n1, prod))
   }
 
-  for _, r := range findThree(20, ns) {
+  for _, r := range findThree(5, ns) {
     n0, n1, n2 := ns[r.i0], ns[r.i1], ns[r.i2]
     sum, prod := n0+n1+n2, n0*n1*n2
     fmt.Println(fmt.Sprintf("indices (%d,%d,%d): %d + %d + %d = %d, %d * %d * %d = %d",
@@ -45,55 +45,16 @@ func main() {
   }
 }
 
-func findTwo(gs int, ns []int) []res2 {
+func findTwo(ns []int) []res2 {
   var results []res2
 
-  inputs := make(chan int, gs)
-  result := make(chan res2, 0)
-  doneSem := make(chan empty, 1)
-
-  wgSem := make(chan empty, 1)
-  var wg sync.WaitGroup
-
-  for gid := 0; gid < gs; gid++ {
-    wg.Add(1)
-    go func() {
-      defer wg.Done()
-      for {
-        i0, more := <-inputs
-        if !more {
-          break
-        }
-        for i1 := 0; i1 < len(ns); i1++ {
-          if ns[i0]+ns[i1] == target {
-            result <- res2{i0, i1}
-          }
-        }
-      }
-    }()
-  }
-
-  go func() {
-    defer func() { doneSem <- empty{} }()
-    loop:
-    for {
-      select {
-      case <-wgSem:
-        break loop
-      case r := <-result:
-        results = append(results, r)
+  for i0 := 0; i0 < len(ns); i0++ {
+    for i1 := i0; i1 < len(ns); i1++ {
+      if ns[i0]+ns[i1] == target {
+        results = append(results, res2{i0, i1})
       }
     }
-  }()
-
-  for i := 0; i < len(ns); i++ {
-    inputs <- i
   }
-  close(inputs)
-
-  wg.Wait()
-  wgSem <- empty{}
-  <-doneSem
 
   return results
 }
@@ -119,8 +80,8 @@ func findThree(gs int, ns []int) []res3 {
         if !more {
           break
         }
-        for i1 := 0; i1 < len(ns); i1++ {
-          for i2 := 0; i2 < len(ns); i2++ {
+        for i1 := i0; i1 < len(ns); i1++ {
+          for i2 := i1; i2 < len(ns); i2++ {
             if ns[i0]+ns[i1]+ns[i2] == target {
               result <- res3{i0, i1, i2}
             }
