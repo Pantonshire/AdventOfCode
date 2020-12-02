5cb5e06310fbe997fb7f9c2f8f3a79fa536b948f
Pantonshire <tom@tomandtally.co.uk>
2020-12-02 10:02:16 +0000

    Day 02 part 2

--- 02/main.hs
+++ 02/main.hs
@@ -8,7 +8,9 @@ type Parser = String -> Maybe (Rule Char)
 main :: IO ()
 main = readFile "input" >>= \contents ->
        let ls = filter (not . null) (lines contents) in
-       putStrLn $ show $ countMaybe id $ checkAll (parse Range) ls
+       putStrLn ("Part 1: " ++ showSatisfiesCount Range ls)  >>
+       putStrLn ("Part 2: " ++ showSatisfiesCount Positions ls)
+     where showSatisfiesCount f ls' = show $ countMaybe id $ checkAll (parse f) ls'
 
 checkAll :: Parser -> [String] -> Maybe [Bool]
 checkAll f ls = foldMaybes (map (check f) ls)
@@ -23,6 +25,7 @@ satisfies :: Eq a => Rule a -> [a] -> Bool
 satisfies (Range x min max) ys = min <= n && n <= max
                                where zs = filter (== x) ys
                                      n = length zs
+satisfies (Positions x i j) ys = xor (occursAtPos ys x (i-1)) (occursAtPos ys x (j-1))
 
 parse :: (Char -> Int -> Int -> Rule Char) -> Parser
 parse g cs = readMaybe f >>= \no1 ->
@@ -50,3 +53,11 @@ foldMaybes ((Just x):mxs) = foldMaybes mxs >>= \xs ->
 
 countMaybe :: (a -> Bool) -> Maybe [a] -> Maybe Int
 countMaybe p mxs = mxs >>= \xs -> return (length (filter p xs))
+
+xor :: Bool -> Bool -> Bool
+xor = (/=)
+
+occursAtPos :: Eq a => [a] -> a -> Int -> Bool
+occursAtPos [] _ _ = False
+occursAtPos (x:_) y 0 = x == y
+occursAtPos (_:xs) y n = occursAtPos xs y (n-1)
