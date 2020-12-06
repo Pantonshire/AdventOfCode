880fcaf85bc98d0428dcc688076bb1ba30d3933b
Pantonshire <tom@tomandtally.co.uk>
2020-12-06 06:12:52 +0000

    Day 06

---
+++ 06/src/main.rs
@@ -0,0 +1,59 @@
+use std::fs;
+
+fn main() {
+    let contents = fs::read_to_string("input").expect("Failed to read file");
+
+    let groups = contents.split("\n\n")
+                         .map(|ls| ls.split("\n")
+                                     .map(|l| l.trim())
+                                     .filter(|l| !l.is_empty()));
+        
+    let (anyone, everyone) = read_all_groups_answers(groups);
+
+    let total_anyone = sum_all_groups_answers(&anyone);
+    println!("Total (anyone): {}", total_anyone);
+
+    let total_everyone = sum_all_groups_answers(&everyone);
+    println!("Total (everyone): {}", total_everyone);
+}
+
+fn read_all_groups_answers<'a, I, II>(groups: I) -> (Vec<u32>, Vec<u32>)
+where
+    II: Iterator<Item = &'a str>,
+    I: Iterator<Item = II>,
+{
+    let mut anyone = Vec::new();
+    let mut everyone = Vec::new();
+
+    for group in groups {
+        let mut any: u32 = 0;
+        let mut every: u32 = 0xFFFFFFFF;
+        for person in group {
+            let mut person_answers = 0;
+            for c in person.chars() {
+                if 'a' <= c && c <= 'z' {
+                    let mask = 1 << (c as u32) - ('a' as u32);
+                    any |= mask;
+                    person_answers |= mask;
+                }
+            }
+            every &= person_answers;
+        }
+        anyone.push(any);
+        everyone.push(every);
+    }
+
+    (anyone, everyone)
+}
+
+fn sum_all_groups_answers(answers: &[u32]) -> u32 {
+    answers.iter().map(|&x| sum_group_answers(x)).sum()
+}
+
+fn sum_group_answers(answers: u32) -> u32 {
+    let mut sum = 0;
+    for i in 0..26 {
+        sum += (answers >> i) & 1;
+    }
+    sum
+}
